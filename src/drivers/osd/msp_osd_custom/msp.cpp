2e7824c42d3d905fe794f2ad213b7e6525f51e74
Andrii Lahuta <13280256+andriilahuta@users.noreply.github.com>
2024-04-18 21:36:25 +0000

    wip

--- src/drivers/osd/msp_osd_custom/msp.cpp
+++ src/drivers/osd/msp_osd_custom/msp.cpp
@@ -88,6 +88,58 @@ msp_osd_buffer MspEncoder::createDataBuffer(const MspCommand command) const {
 }
 
 msp_osd_buffer MspEncoder::createDataBuffer(const MspStatus status) const {
+    /*
+    MSP:
+        uint16_t cycleTime;
+        uint16_t i2cErrorsCount;
+        uint16_t sensor;
+        uint32_t flag;
+        uint8_t currentSetting;
+    */
+
+
+
+            // boxBitmask_t flightModeFlags;
+            // const int flagBits = packFlightModeFlags(&flightModeFlags);
+
+            // sbufWriteU16(dst, getTaskDeltaTimeUs(TASK_PID));
+            // sbufWriteU16(dst, i2cGetErrorCounter());
+            // sbufWriteU16(dst, sensors(SENSOR_ACC) | sensors(SENSOR_BARO) << 1 | sensors(SENSOR_MAG) << 2 | sensors(SENSOR_GPS) << 3 | sensors(SENSOR_RANGEFINDER) << 4 | sensors(SENSOR_GYRO) << 5);
+            // sbufWriteData(dst, &flightModeFlags, 4);        // unconditional part of flags, first 32 bits
+            // sbufWriteU8(dst, getCurrentPidProfileIndex());
+            // sbufWriteU16(dst, constrain(getAverageSystemLoadPercent(), 0, LOAD_PERCENTAGE_ONE));
+            // if (cmdMSP == MSP_STATUS_EX) {
+            //     sbufWriteU8(dst, PID_PROFILE_COUNT);
+            //     sbufWriteU8(dst, getCurrentControlRateProfileIndex());
+            // } else {  // MSP_STATUS
+            //     sbufWriteU16(dst, 0); // gyro cycle time
+            // }
+
+            // // write flightModeFlags header. Lowest 4 bits contain number of bytes that follow
+            // // header is emited even when all bits fit into 32 bits to allow future extension
+            // int byteCount = (flagBits - 32 + 7) / 8;        // 32 already stored, round up
+            // byteCount = constrain(byteCount, 0, 15);        // limit to 16 bytes (128 bits)
+            // sbufWriteU8(dst, byteCount);
+            // sbufWriteData(dst, ((uint8_t*)&flightModeFlags) + 4, byteCount);
+
+            // // Write arming disable flags
+            // // 1 byte, flag count
+            // sbufWriteU8(dst, ARMING_DISABLE_FLAGS_COUNT);
+            // // 4 bytes, flags
+            // const uint32_t armingDisableFlags = getArmingDisableFlags();
+            // sbufWriteU32(dst, armingDisableFlags);
+
+            // // config state flags - bits to indicate the state of the configuration, reboot required, etc.
+            // // other flags can be added as needed
+            // sbufWriteU8(dst, (getRebootRequired() << 0));
+
+            // // Added in API version 1.46
+            // // Write CPU temp
+            // sbufWriteU16(dst, getCoreTemperatureCelsius());
+
+
+
+
     return msp_osd_buffer {};
 }
 
@@ -152,8 +204,7 @@ msp_osd_buffer MspEncoder::encodeV1(msp_osd_buffer& headerBuff, const msp_osd_bu
     headerBuff.push_back(static_cast<std::byte>(command));
 
     if (dataSize >= MSP_JUMBO_FRAME_SIZE_LIMIT) {
-        msp_osd_buffer jumboSize = msp_osd_utils::to_bytes(static_cast<uint16_t>(dataSize), true);
-        headerBuff.insert(headerBuff.end(), jumboSize.begin(), jumboSize.end());
+        headerBuff.append_range(msp_osd_utils::to_bytes(static_cast<uint16_t>(dataSize), true));
     }
 
     std::byte checksum = mspSerialChecksumBuf(std::span {headerBuff.begin() + 3, headerBuff.end()});
@@ -177,15 +228,12 @@ msp_osd_buffer MspEncoder::encodeV2overV1(msp_osd_buffer& headerBuff, const msp_
     msp_osd_buffer headerV2Buff {
         std::byte {},  // flags byte
     };
-    msp_osd_buffer hdrV2Cmd = msp_osd_utils::to_bytes(static_cast<uint16_t>(command), true);
-    msp_osd_buffer hdrV2Size = msp_osd_utils::to_bytes(static_cast<uint16_t>(dataBuff.size()), true);
-    headerV2Buff.insert(headerV2Buff.end(), hdrV2Cmd.begin(), hdrV2Cmd.end());
-    headerV2Buff.insert(headerV2Buff.end(), hdrV2Size.begin(), hdrV2Size.end());
-    headerBuff.insert(headerBuff.end(), headerV2Buff.begin(), headerV2Buff.end());
+    headerV2Buff.append_range(msp_osd_utils::to_bytes(static_cast<uint16_t>(command), true));
+    headerV2Buff.append_range(msp_osd_utils::to_bytes(static_cast<uint16_t>(dataBuff.size()), true));
+    headerBuff.append_range(headerV2Buff);
 
     if (payloadSize >= MSP_JUMBO_FRAME_SIZE_LIMIT) {
-        msp_osd_buffer jumboSize = msp_osd_utils::to_bytes(static_cast<uint16_t>(payloadSize), true);
-        headerBuff.insert(headerBuff.end(), jumboSize.begin(), jumboSize.end());
+        headerBuff.append_range(msp_osd_utils::to_bytes(static_cast<uint16_t>(payloadSize), true));
     }
 
     std::byte v2_checksum = crc8DvbS2Update(headerV2Buff);
@@ -211,11 +259,9 @@ msp_osd_buffer MspEncoder::encodeV2(msp_osd_buffer& headerBuff, const msp_osd_bu
         uint16_t cmd;
         uint16_t size;
     */
-    msp_osd_buffer hdrV2Cmd = msp_osd_utils::to_bytes(static_cast<uint16_t>(command), true);
-    msp_osd_buffer hdrV2Size = msp_osd_utils::to_bytes(static_cast<uint16_t>(dataBuff.size()), true);
     headerBuff.push_back(std::byte {});  // flags byte
-    headerBuff.insert(headerBuff.end(), hdrV2Cmd.begin(), hdrV2Cmd.end());
-    headerBuff.insert(headerBuff.end(), hdrV2Size.begin(), hdrV2Size.end());
+    headerBuff.append_range(msp_osd_utils::to_bytes(static_cast<uint16_t>(command), true));
+    headerBuff.append_range(msp_osd_utils::to_bytes(static_cast<uint16_t>(dataBuff.size()), true));
 
     std::byte checksum = crc8DvbS2Update(std::span {headerBuff.begin() + 3, headerBuff.end()});
     checksum = crc8DvbS2Update(dataBuff, checksum);
